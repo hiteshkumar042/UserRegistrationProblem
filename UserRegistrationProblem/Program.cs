f4c6377ca1a33e823717281590f270f46074e463
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-10 20:30:32 +0530

    UC1-FirstName Added

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message  
+            Console.WriteLine("========================================\nWelcome To the User Registration Problem\n========================================");
+            RegistrationValidate registrationValidate= new RegistrationValidate();
+            registrationValidate.ValidateFirstName("Hitesh");
+            registrationValidate.ValidateFirstName("hitesh");
+
+        }
+    }
+}
---
+++ UserRegistrationProblem/RegistrationValidate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UserRegistrationProblem
+{
+    internal class RegistrationValidate
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+
+        public void ValidateFirstName(string name)
+        {
+            string firstnameregex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regexfirst = new Regex(firstnameregex);
+            if (regexfirst.IsMatch(name))
+            {
+                Console.WriteLine(name + " : Valid");
+            }
+            else
+            {
+                Console.WriteLine(name +" : Invalid");        
+            }
+        }
+    }
+}
